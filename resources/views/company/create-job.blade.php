bbb38ef512aee976ec9cba360da97371d878f1e8
RodrigoVitor <rodrigovitordev2024@gmail.com>
2025-10-08 09:17:57 -0300

    bugfix: removendo placeholder

--- resources/views/company/create-job.blade.php
+++ resources/views/company/create-job.blade.php
@@ -21,7 +21,7 @@
                 <!-- company name -->
                 <div class="mb-4">
                     <label for="name" class="block text-sm font-medium text-white mb-1">Nome da empresa</label>
-                    <input type="text" id="name" name="name" placeholder="Ex: Auxiliar de logistica"
+                    <input type="text" id="name" name="name" 
                         class="w-full px-4 py-2 border rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
                         required />
                 </div>
