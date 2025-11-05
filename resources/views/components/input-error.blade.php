47bef6d921bf13025f03f02b01837769cd1156f4
RodrigoVitor <rodrigovitordev2024@gmail.com>
2025-11-05 11:33:01 -0300

    bugfix: mudando cor do texto para branco

--- resources/views/components/input-error.blade.php
+++ resources/views/components/input-error.blade.php
@@ -1,7 +1,7 @@
 @props(['messages'])
 
 @if ($messages)
-    <ul {{ $attributes->merge(['class' => 'text-sm text-red-600 dark:text-white bg-red-500 p-2 space-y-1']) }}>
+    <ul {{ $attributes->merge(['class' => 'text-sm text-white dark:text-white bg-red-500 p-2 space-y-1']) }}>
         @foreach ((array) $messages as $message)
             <li>{{ $message }}</li>
         @endforeach
